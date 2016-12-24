10b09c57123ea1536b9e7212c0634cbaf5e5cd66
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2016-12-24 11:58:34 +0530

    bricks added

--- scripts.js
+++ scripts.js
@@ -18,13 +18,67 @@ var paddleWidth = 75;
 var paddleX = (canvas.width - paddleWidth) / 2;
 var leftPressed = false;
 var rightPressed = false;
-var paddleMovement = 7;
+var paddleOffset = 7;
 
 var leftArrowKeyCode = 37;
 var rightArrowKeyCode = 39;
 
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+var brickX = 0, brickY = 0;
+
 document.addEventListener('keydown', keyDownHandler);
-document.addEventListener('keyup', keyUpHandler)
+document.addEventListener('keyup', keyUpHandler);
+
+for(var c = 0; c < brickColumnCount; c++){
+  bricks[c] = [];
+  for(var r = 0; r < brickRowCount; r++){
+    bricks[c][r] = {x: 0, y: 0, status: false};
+  }
+}
+
+var brick;
+function collisionDetection(){
+  for(var c = 0; c < brickColumnCount; c++){
+    for(var r = 0; r < brickRowCount; r++){
+      brick = bricks[c][r];
+      if(!bricks[c][r].status){
+        if(x > brick.x && x < brick.x + brickWidth && y > brick.y && y < brick.y + brickHeight){
+          dy = -dy;
+          brick.status = true;
+        }
+      }
+    }
+  }
+}
+
+function drawBrick(x, y){
+  ctx.beginPath();
+  ctx.rect(x, y, brickWidth, brickHeight);
+  ctx.fillStyle = "purple";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawBricks(){
+  for(var c = 0; c < brickColumnCount; c++){
+    for(var r = 0; r < brickRowCount; r++){
+      if(bricks[c][r].status) continue;
+      brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+      brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+      bricks[c][r].x = brickX;
+      bricks[c][r].y = brickY;
+      drawBrick(brickX, brickY);
+    }
+  }
+}
 
 function keyUpHandler(e){
   if(e.keyCode === leftArrowKeyCode) leftPressed = false;
@@ -56,8 +110,10 @@ function drawPaddle(){
 
 function draw(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks();
   drawBall();
   drawPaddle();
+  collisionDetection();
   sumX = x + dx; sumY = y + dy;
   if(sumY < ballRadius){
     dy = -dy;
@@ -69,7 +125,7 @@ function draw(){
     }
   }
   if(sumX > (canvas.width - ballRadius) || sumX < ballRadius) dx = -dx;
-  if(leftPressed && paddleX > 0) paddleX -= paddleMovement;
-  if(rightPressed && paddleX + paddleWidth < canvas.width) paddleX += paddleMovement;
+  if(leftPressed && paddleX > 0) paddleX -= paddleOffset;
+  if(rightPressed && paddleX + paddleWidth < canvas.width) paddleX += paddleOffset;
   x += dx; y += dy;
 }
